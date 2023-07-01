7a15d564687c05a21310a9e43b230fccb253cf70
Abhishek Jain <abhishek.j0407@gmail.com>
2023-07-01 21:32:42 +0530

    Production build added to server

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+dotenv.config();
 import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import userRoutes from './routes/userRoutes.js';
 
-
-dotenv.config();
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -18,8 +18,20 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.get('/', (req, res) => res.send('API running'));
 app.use('/api/users', userRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
